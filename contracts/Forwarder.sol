d4bfe7af260e4f0d223b65bafb491e16aa3a2b66
Dr. Savage <78974706+Egavasyug@users.noreply.github.com>
2025-09-22 17:42:07 -0400

    feat(contracts): add CamuverseForwarder (OZ MinimalForwarder wrapper) for ERC-2771

---
+++ contracts/Forwarder.sol
@@ -0,0 +1,8 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
+
+/// @title CamuverseForwarder
+/// @notice OZ MinimalForwarder wrapper used for ERC-2771 meta-transactions
+contract CamuverseForwarder is MinimalForwarder {}
